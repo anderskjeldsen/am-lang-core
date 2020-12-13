d3618af2ab3c0c703600e9759c6acd779437266c
Anders Kjeldsen <anders@kelson.no>
2020-12-13 20:57:47 +0100

    Initial commit

---
+++ src/native-c/amigaos/Am/Lang/Bool.c
@@ -0,0 +1,7 @@
+#ifndef native_aclass_Am_Lang_Bool_c
+#define native_aclass_Am_Lang_Bool_c
+#include <asharp.h>
+#include <Am/Lang/Bool.h>
+#include <Am/Lang/Object.h>
+
+#endif
---
+++ src/native-c/amigaos/Am/Lang/Long.c
@@ -0,0 +1,7 @@
+#ifndef native_aclass_Am_Lang_Long_c
+#define native_aclass_Am_Lang_Long_c
+#include <asharp.h>
+#include <Am/Lang/Long.h>
+#include <Am/Lang/Object.h>
+
+#endif
