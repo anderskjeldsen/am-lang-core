5489a8a2a92fed8ad528714e1f72e9d246204719
anderskjeldsen <mbp2nfuqoku3zbj2pkbicrgsx24kq7p2voq5mm4dxgli7yfhvtha>
2022-02-15 12:22:53 +0100

    added toLong function in Int

--- src/native-c/libc/Am/Lang/Int.c
+++ src/native-c/libc/Am/Lang/Int.c
@@ -29,4 +29,15 @@ __exit: ;
 	return __result;
 };
 
+function_result Am_Lang_Int_toLong_0(int const this)
+{
+	function_result __result = { .has_return_value = true };
+	bool __returning = false;
+
+	__result.return_value = (nullable_value) { .value = { .long_value = this }, .flags = 0 };
+	
+__exit: ;
+	return __result;
+};
+
 #endif
