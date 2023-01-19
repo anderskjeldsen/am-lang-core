739835c97ae00081cf5181e8cef503a69284e67c
Anders Kjeldsen <kjeldsenanders@gmail.com>
2023-01-19 17:43:20 +0100

    more files

---
+++ src/native-c/morphos-ppc/Am/IO/Networking/Socket.c
@@ -0,0 +1,6 @@
+#include <libc/core.h>
+#include <Am/IO/Networking/Socket.h>
+#include <morphos-ppc/Am/IO/Networking/Socket.h>
+#include <Am/Lang/Object.h>
+#include <Am/IO/Networking/AddressFamily.h>
+
