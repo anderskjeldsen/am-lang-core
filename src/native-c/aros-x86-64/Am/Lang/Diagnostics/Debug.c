963764f2427d8cb55585cfd21d0e020010fa6b63
Anders Kjeldsen <anders@appify.no>
2024-12-03 22:29:47 +0100

    debug

---
+++ src/native-c/aros-x86-64/Am/Lang/Diagnostics/Debug.c
@@ -0,0 +1,8 @@
+#include <libc/core.h>
+#include <Am/Lang/Diagnostics/Debug.h>
+#include <aros-x86-64/Am/Lang/Diagnostics/Debug.h>
+#include <Am/Lang/ClassRef.h>
+#include <Am/Lang/Object.h>
+#include <Am/Lang/Bool.h>
+#include <libc/core_inline_functions.h>
+
