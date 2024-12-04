565417f22523c1114dfabd97288881bcb4c69728
anderskjeldsen <mbp2nfuqoku3zbj2pkbicrgsx24kq7p2voq5mm4dxgli7yfhvtha>
2024-12-04 10:11:18 +0100

    wip

--- src/native-c/libc/core_inline_functions.h
+++ src/native-c/libc/core_inline_functions.h
@@ -98,6 +98,11 @@ static inline void __decrease_property_reference_count(aobject * const __obj) {
             printf("decrease property reference count of object of type %s (address: %p, object_id: %d), new reference count %d\n", __obj->class_ptr->name, __obj, __obj->object_properties.class_object_properties.object_id, __obj->reference_count);
         }
 
+        if (strcmp(__obj->class_ptr->name, "Am_Lang_String") == 0) {
+            string_holder *sh = (string_holder *) __obj->object_properties.class_object_properties.object_data.value.custom_value;
+            printf("String value: %s\n", sh->string_value);
+        }
+
         if (__obj->property_reference_count == 0) {
             if (__obj == __first_object) {
                 __first_object = __obj->next;                
