2daebafe27ec680dd3eac010da20dacc849c14fe
Anders Kjeldsen <anders@appify.no>
2025-05-11 22:38:00 +0200

    wip

--- src/native-c/libc/Am/Lang/String.c
+++ src/native-c/libc/Am/Lang/String.c
@@ -267,7 +267,7 @@ function_result Am_Lang_String_toBytes_0(aobject * const this, aobject * encodin
 		__increase_reference_count(encoding);
 	}
 	string_holder *string_holder = this->object_properties.class_object_properties.object_data.value.custom_value;
-	aobject *array = __create_array(string_holder->length, 1, &Am_Lang_Array_v_uchar, uchar_type);
+	aobject *array = __create_array(string_holder->length, 1, &Am_Lang_Array_ta_Am_Lang_UByte, uchar_type);
 
 	array_holder *a_holder = (array_holder *) &array[1]; // array->object_properties.class_object_properties.object_data.value.custom_value;
 	strcpy(a_holder->array_data, string_holder->string_value);
