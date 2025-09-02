f813a3dde00fe6640ef657ab087f6e1988dbe6ca
Anders Kjeldsen <anders@appify.no>
2025-09-02 22:15:42 +0200

    Random natives

---
+++ src/native-c/amigaos/Am/Util/Random.c
@@ -0,0 +1,8 @@
+#include <libc/core.h>
+#include <Am/Util/Random.h>
+#include <amigaos/Am/Util/Random.h>
+#include <Am/Lang/ClassRef.h>
+#include <Am/Lang/Int.h>
+#include <Am/Lang/Object.h>
+#include <libc/core_inline_functions.h>
+
---
+++ src/native-c/amigaos/Am/Util/Random.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <libc/core.h>
+#include <Am/Util/Random.h>
+#include <Am/Lang/ClassRef.h>
+#include <Am/Lang/Int.h>
+#include <Am/Lang/Object.h>
