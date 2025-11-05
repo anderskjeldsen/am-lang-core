c579bd834a1cf8e5e703f1cebb8f9a746e51dcfe
Anders Kjeldsen <anders@appify.no>
2025-11-05 20:08:24 +0100

    Temp files deleted

--- src/native-c/linux-x64/Am/IO/TempFile.c
+++
@@ -1,9 +0,0 @@
-#include <libc/core.h>
-#include <Am/IO/TempFile.h>
-#include <linux-x64/Am/IO/TempFile.h>
-#include <Am/Lang/ClassRef.h>
-#include <Am/Lang/Object.h>
-#include <Am/IO/File.h>
-#include <Am/Lang/String.h>
-#include <libc/core_inline_functions.h>
-
--- src/native-c/macos-arm/Am/IO/TempFile.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <libc/core.h>
-#include <Am/IO/TempFile.h>
-#include <Am/Lang/ClassRef.h>
-#include <Am/Lang/Object.h>
-#include <Am/IO/File.h>
-#include <Am/Lang/String.h>
