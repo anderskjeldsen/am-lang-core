4c8f0eb2e1dc2322e9a0f0c286d5c841276f48bd
Anders Kjeldsen <anders@appify.no>
2026-05-15 07:34:21 +0200

    remove old files

--- src/native-c/amigaos/Am/IO/Networking/Socket.c
+++
@@ -1,7 +0,0 @@
-#include <libc/core.h>
-#include <Am/IO/Networking/Socket.h>
-#include <amigaos/Am/IO/Networking/Socket.h>
-#include <Am/Lang/Object.h>
-#include <Am/IO/Networking/AddressFamily.h>
-#include <libc/core_inline_functions.h>
-
--- src/native-c/amigaos/Am/IO/Networking/Socket.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <libc/core.h>
-#include <Am/IO/Networking/Socket.h>
-#include <Am/Lang/Object.h>
-#include <Am/IO/Networking/AddressFamily.h>
-#include <libc/core_inline_functions.h>
